5c46863ace09ede20b2751fe937a6d0cd8cdd965
JaviQuinho <javier.qg@hotmail.com>
2021-10-14 11:22:22 +0200

    Modify constructors

--- src/main/java/com/form/heroesBack/mission/Mission.java
+++ src/main/java/com/form/heroesBack/mission/Mission.java
@@ -1,19 +1,23 @@
 package com.form.heroesBack.mission;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Mission {
 
-@Id
-@GeneratedValue
-private long id;
-@Column(nullable=false)
-private String instructions;
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false)
+    private String instructions;
 }
