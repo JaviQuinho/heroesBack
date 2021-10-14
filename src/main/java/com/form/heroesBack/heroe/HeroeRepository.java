1291b6083446ec23bbd22840b1575832a12c5a01
JaviQuinho <javier.qg@hotmail.com>
2021-10-14 11:23:25 +0200

    JPA. New service by name

--- src/main/java/com/form/heroesBack/heroe/HeroeRepository.java
+++ src/main/java/com/form/heroesBack/heroe/HeroeRepository.java
@@ -1,7 +1,12 @@
 package com.form.heroesBack.heroe;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(excerptProjection = Heroe.class)
-public interface HeroeRepository extends CrudRepository<Heroe, Long>{
+public interface HeroeRepository extends CrudRepository<Heroe, Long> {
+    public List<Heroe> findByFirstNameStartingWithIgnoreCase(@Param("firstName") String firstName);
 }
